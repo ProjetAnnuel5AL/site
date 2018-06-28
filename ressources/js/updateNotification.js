04a666491936ce5f63c8a0076f01d4ddf96a9d96
andreESGI <andre92.morel@gmail.com>
2018-06-28 22:42:51 +0200

    CoopAgriculteur

---
+++ ressources/js/updateNotification.js
@@ -0,0 +1,43 @@
+function updateNotif(userLogin){
+  $.getJSON("/notification/userLogin", { userLogin: userLogin }).done(function (json) {
+    console.log(json);
+    var element;
+    if (json.code == 0) {
+      if(json.result.length > 0){
+        $('#imageNotif').css("display", "inline");
+      }else{
+        $('#imageNotif').css("display", "none");
+      }
+      $('#notifLabel').text("Notifications ("+json.result.length+")");
+      $('#notif-sidebar').empty();
+      for (var elem in json.result) {
+        element = "<li class='itemNotif'id='Notif"+json.result[elem].id+"'>" +
+          "<div class='item-inner'>" +
+          "<div>" +
+          "<div style='margin-left: 10px;'>" +
+          "<strong>" + json.result[elem].title + "</strong><br><span>" + json.result[elem].description + "</span>" +
+          "</div>" +
+          "<div>";
+          if(json.result[elem].type=="info"){
+            element += "<button class='btnInfo' title='Ok' type='button' onclick='deleteNotif("+json.result[elem].id+");'><span>Ok</span></a></button>"; 
+          }else{
+          element += "<button class='btnValidate' title='Accept' type='button'><a class='aNotif' href='"+json.result[elem].url+"'><span>Accepter</span></a></button>" +
+          "<button class='btnRefuse' title='Refuse' type='button' onclick='deleteNotif("+json.result[elem].id+");'><span>Refuser</span> </button>";
+          }
+          element += "</div>" +
+          "</div>" +
+          "</div>" +
+          "</li>";
+        $('#notif-sidebar').append(element);
+      }
+    }
+  });
+}
+
+function deleteNotif(id){
+  $.getJSON("/notification/delete/idNotif", { id: id }).done(function (json) {
+    if (json.code == 0) {
+      updateNotif('<%- session.login %>');
+    }
+  });
+}
